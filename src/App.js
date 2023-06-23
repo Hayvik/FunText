b10ac8cd7a50d2d1cfa66edbbce3978c26fcebab
Hayvik <shitlayadavvk@gmail.com>
2023-06-23 23:34:01 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from "react";
+import Bootsrap from './components/Navbar.js'
+import Footer from "./components/Footer.js";
+let name='Text Converter'
+let home='Homepage'
+export default function App(){
+  return(
+  <>
+    {/* <h1>Hello {name}</h1>
+    
+    <p>This is vikas</p> */}
+    <Bootsrap name={name} home={home}/>
+    <Footer/>
+    
+    
+  </>
+  )
 }
-
-export default App;
